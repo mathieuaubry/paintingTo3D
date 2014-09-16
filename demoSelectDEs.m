5091026db64c45d508df0afdd38929fa66266f25
mathieuaubry <aubry.mathieu@gmail.com>
2014-09-16 14:12:05 +0200

    Update demoSelectDEs.m

--- demoSelectDEs.m
+++ demoSelectDEs.m
@@ -1,18 +1,3 @@
-%% TODO
-% nead to have a way to read vertices and faces
-
-
-addpath('/meleze/data0/libs/APT');
-global APT_PARAMS;
-if isempty(APT_PARAMS)
-    APT_params();
-end
-
-root = fileparts(mfilename('fullpath'));
-[~, f] = fileparts(root);
-APT_PARAMS.exec_name = f;
-%APT_compile();
-
 
 addpath('./code');
 init_DE_params;
@@ -24,8 +9,7 @@ ELTS_DIR='/meleze/data1/maaubry/paintings_release/cache_san_marco_sample_30_up_1
 mkdir(ELTS_DIR);
 
 
-indices_to_visit=randperm( (length(CameraStruct)));
-%getDiscriminativElements(1,MODEL_DIR,ELTS_DIR,CameraStruct,DE_params,view_params);
-%APT_run('getDiscriminativElements',indices_to_visit',{MODEL_DIR},{ELTS_DIR},{CameraStruct},{DE_params},{view_params},'UseCluster', 1, 'ClusterID',1,'NJobs', 50 );
+indices_to_visit=randperm( (length(CameraStruct))); %this compute all DEs. In practice, you can take less by indices_to_visit=indices_to_visit(1:500)
+getDiscriminativElements(1,MODEL_DIR,ELTS_DIR,CameraStruct,DE_params,view_params);
 
 summarizeDEs(ELTS_DIR,MODEL_DIR,indices_to_visit)
