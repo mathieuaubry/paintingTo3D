69dee19f2ab2bc299d49a1efd83f1e1a56c62820
mathieuaubry <aubry.mathieu@gmail.com>
2014-09-16 13:58:24 +0200

    Update compile.m

--- compile.m
+++ compile.m
@@ -1,11 +1,3 @@
 
-  % Compile trimesh2:
-  cd('./code/LIBS/trimesh2');
-  system('make');
-  cd('../../..');
-  
-  % Compile OpenGL rendering code:
-  cd('./code/LIBS/GenerateLambertian');
-  system('make');
-  cd('../../..');
-  
+ addpath('./code');
+ mex features.cc;
