319763ec2323aecae7b338d8618ac96c8c93d4aa
Bryan Russell <bcr@cs.washington.edu>
2014-09-26 00:59:50 -0700

    added instructions for demoRendering

--- demoRendering.m
+++ demoRendering.m
@@ -1,3 +1,68 @@
+%{
+
+% Instructions for running rendering code.
+
+% Step 1: Download trimesh2:
+% http://gfx.cs.princeton.edu/proj/trimesh2/src/trimesh2-2.12.tar.gz
+%
+% Step 2: Unzip trimesh2 tarball and put inside ./renderer/LIBS/
+% 
+% Step 3 (Mac): Compile mex from within Matlab:
+%
+% cd ./renderer/
+% mex mexReadPly.cpp -I./LIBS/trimesh2/include -L./LIBS/trimesh2/lib.Darwin64 -ltrimesh -lgomp
+%
+% If you get errors saying that the compiler cannot find libgomp, then
+% you may be using a CLANG compiler (recent versions of Mac use this),
+% which does not contain openmp.  If this is the case, then try the following:
+%
+% 3a. Install MacPorts and run the following in the bash shell:
+%
+% sudo port install gcc49
+%
+% 3b. Change the "CXX" variable assignment in Matlab's "mexopts" (/Applications/MATLAB_R2014a.app/bin/maci64/mexopts/clang++_maci64.xml:
+%
+%   CXX="$XCRUN_DIR/xcrun /opt/local/bin/g++-mp-4.9"
+%
+% 3c. Run original Step 3 (Mac) above.
+%
+% Step 3 (Linux): Compile mex from within Matlab:
+%
+% cd ./renderer/
+% mex mexReadPly.cpp -I./LIBS/trimesh2/include -L./LIBS/trimesh2/lib.Linux64 -ltrimesh -lgomp
+%
+% If you get an error saying to recompile with -fPIC, then you need to
+% recompile trimesh2:
+%
+% 3a. Run inside bash shell:
+%
+% cd ./renderer/LIBS/trimesh2/
+% make ARCHOPTS=-fPIC
+%
+% 3b. Run original Step 3 (Linux) above.
+%
+% Step 4: Compile "get_3d_info_textures" by running on Bash shell:
+%
+% cd ./renderer/
+% make
+%
+% Step 5: Run the code below.  It should display a rendered view.  
+%
+% If you're running on Linux and the code hangs, first make sure you can
+% display X windows (i.e. if you're running over SSH make sure you have X
+% forwarding enabled; you can test this by running "xterm" in the bash
+% shell).  If you can display X windows but the code hangs, then 
+% run the following in the Bash shell:
+%
+% export LIBGL_ALWAYS_INDIRECT=1
+%
+% Then try to run the code below again.
+
+%}
+
+
+% Once everything is compiled, run the following to render a scene.
+
 addpath ./renderer;
 addpath ./code;
 
@@ -7,29 +72,5 @@ meshFileName = 'out_model_venice';
 
 img = meshGenerateColored(P,meshFileName,size(I),BIN_PATH);
 
-
-%{
-
-% Recent versions of Mac use clang for compilation, which does not
-% contain openmp (libgomp).
-%
-% Steps to compile using openmp:
-%
-% 1. sudo port install gcc49
-% 2. Change the following in Matlab's "mexopts" (/Applications/MATLAB_R2014a.app/bin/maci64/mexopts/clang++_maci64.xml:
-%   CXX="$XCRUN_DIR/xcrun /opt/local/bin/g++-mp-4.9"
-
-% To compile mex file from within Matlab:
-mex mexReadPly.cpp -I./LIBS/trimesh2/include -L./LIBS/trimesh2/lib.Darwin64 -ltrimesh -lgomp
-
-% To compile mex file from command line:
-/Applications/MATLAB_R2014a.app/bin/mex -v mexReadPly.cpp -I./LIBS/trimesh2/include -L./LIBS/trimesh2/lib.Darwin64 -ltrimesh -lgomp
-
-
-
-
-./code/get_3d_info_textures out_model_venice /private/tmp/tpefb43a89_0f2c_4c89_9fb2_813f8bece6ec_pipe.txt 599 467 /tmp/foo_out.txt --texture 72
-
-
-
-%}
+figure;
+imshow(img);
